7399a905f312b49437ce1ff5c0ee6a2bae4774fa
carliys <cflores1833@yahoo.com>
2023-04-11 10:47:51 -0700

    1

--- Develop/server.js
+++ Develop/server.js
@@ -36,26 +36,6 @@ app.post("/api/notes",(req,res)=>{
        })}
     })
 })
-app.delete("/api/notes/:id", function (req, res) {
-    console.log(uuidv1())
-    console.log("Req.params:", req.params);
-    let deletedNote = parseInt(req.params.id);
-    console.log(deletedNote);
-
-    for (let i = 0; i < dbJson.length; i++) {
-        if (deletedNote === dbJson[i].id) {
-            dbJson.splice(i, 1);
-
-            let noteJson = JSON.stringify(dbJson, null, 2);
-            console.log(noteJson);
-            fs.writeFile("./db/db.json", noteJson, function (err) {
-                if (err) throw err;
-                console.log("note has been deleted!");
-                res.json(dbJson);
-            });
-        }
-    }
-});
 app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"/public/index.html"))
 })
