6077d35bf52ea7c0c495f65b914127738d8e2400
carliys <cflores1833@yahoo.com>
2023-04-11 10:47:31 -0700

    trying to figiure out why its not working

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,7 @@ const PORT = 3000
 const path = require ("path")
 const fs = require ("fs")
 const uuid = require ("uuid")
+var uuidv1 = require('uuidv1')
 
 app.use(express.json())
 app.use(express.static("public"))
@@ -35,6 +36,26 @@ app.post("/api/notes",(req,res)=>{
        })}
     })
 })
+app.delete("/api/notes/:id", function (req, res) {
+    console.log(uuidv1())
+    console.log("Req.params:", req.params);
+    let deletedNote = parseInt(req.params.id);
+    console.log(deletedNote);
+
+    for (let i = 0; i < dbJson.length; i++) {
+        if (deletedNote === dbJson[i].id) {
+            dbJson.splice(i, 1);
+
+            let noteJson = JSON.stringify(dbJson, null, 2);
+            console.log(noteJson);
+            fs.writeFile("./db/db.json", noteJson, function (err) {
+                if (err) throw err;
+                console.log("note has been deleted!");
+                res.json(dbJson);
+            });
+        }
+    }
+});
 app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"/public/index.html"))
 })
